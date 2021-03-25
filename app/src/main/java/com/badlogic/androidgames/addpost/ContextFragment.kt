910fd488c0b2579b3a59db3d3aa948a948f717c6
hakan çakmak <hakan.cakmak@live.com>
2021-03-25 16:09:00 +0300

    tasarım çalışıldı. dinleyici metotlar eklendi. test yapıldı.

--- app/src/main/java/com/badlogic/androidgames/addpost/ContextFragment.kt
+++ app/src/main/java/com/badlogic/androidgames/addpost/ContextFragment.kt
@@ -5,6 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import kotlinx.android.synthetic.main.fragment_context.*
 
 
 class ContextFragment : Fragment() {
@@ -18,5 +19,22 @@ class ContextFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_context, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        btnKaydet.setOnClickListener {
+            Kaydet(it)
+        }
+    }
+
+    fun Kaydet(view:View) {
+        println("kayıt butonuna tıklandı.")
+
+    }
+
+    fun ResimSec(view:View) {
+
     }
 }
