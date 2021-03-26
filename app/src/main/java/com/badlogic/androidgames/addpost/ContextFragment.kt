4ebd2f3636c765ad6ea44b735046cfab96d0395f
hakan çakmak <hakan.cakmak@live.com>
2021-03-26 14:32:05 +0300

    permission eklendi. onRequestPermissionsResult ve onActivityResult override edildi. Ancak metotların kodlanması tamamlanmadı

--- app/src/main/java/com/badlogic/androidgames/addpost/ContextFragment.kt
+++ app/src/main/java/com/badlogic/androidgames/addpost/ContextFragment.kt
@@ -1,14 +1,31 @@
 package com.badlogic.androidgames.addpost
 
+import android.app.Activity
+import android.content.Intent
+import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.ImageDecoder
+import android.net.Uri
+import android.os.Build
 import android.os.Bundle
+import android.provider.MediaStore
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 import kotlinx.android.synthetic.main.fragment_context.*
+import java.lang.Exception
+import java.util.jar.Manifest
 
 
 class ContextFragment : Fragment() {
+
+    var secilenGorsel: Uri? = null
+    var secilenBitmap: Bitmap? = null
+    var secilenImage: ImageView? = null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,12 +46,60 @@ class ContextFragment : Fragment() {
         }
     }
 
+    override fun onRequestPermissionsResult(requestCode: Int, permissions: Array<out String>, grantResults: IntArray) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
+
+        if(requestCode == 1) {
+            // İzni Aldık
+            if(grantResults.size > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                val galeryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeryIntent,2)
+            }
+        }
+    }
+
+    override fun onActivityResult(requestCode: Int, resultCode: Int, data: Intent?) {
+        super.onActivityResult(requestCode, resultCode, data)
+
+        if(requestCode == 2 && resultCode == Activity.RESULT_OK && data != null) {
+            secilenGorsel = data.data
+
+            try {
+                context?.let {
+                    if(secilenGorsel != null) {
+                        if (Build.VERSION.SDK_INT >= 28) {
+                            val source = ImageDecoder.createSource(it.contentResolver, secilenGorsel!!)
+                            secilenBitmap = ImageDecoder.decodeBitmap(source)
+                            //secilenImage!!.setImageBitmap(secilenBitmap)
+                        }
+                    }
+                }
+
+            } catch (e: Exception) {
+                e.printStackTrace()
+            }
+        }
+
+    }
+
     fun Kaydet(view:View) {
         println("kayıt butonuna tıklandı.")
 
     }
 
     fun ResimSec(view:View) {
+
+        activity?.let {
+            if(ContextCompat.checkSelfPermission(it.applicationContext, android.Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                requestPermissions(arrayOf(android.Manifest.permission.READ_EXTERNAL_STORAGE),1)
+
+            } else {
+                val galeryIntent = Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                startActivityForResult(galeryIntent,2)
+            }
+        }
+
+
 
     }
 }
