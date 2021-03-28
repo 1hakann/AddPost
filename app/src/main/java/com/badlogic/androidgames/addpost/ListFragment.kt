f8d0649af363d521163ee97dac784ae3280179b3
hakan çakmak <hakan.cakmak@live.com>
2021-03-28 18:48:15 +0300

    ListFragment.kt tamamlandı. SQLite bağlantısı sağlandı. recycler_row oluşturduk. Adapter ümüzü inşa ettik.

--- app/src/main/java/com/badlogic/androidgames/addpost/ListFragment.kt
+++ app/src/main/java/com/badlogic/androidgames/addpost/ListFragment.kt
@@ -1,14 +1,19 @@
 package com.badlogic.androidgames.addpost
 
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.navigation.Navigation
+import java.lang.Exception
 
 
 class ListFragment : Fragment() {
+
+    val postIdListesi = ArrayList<Int>()
+    val postBaslikListesi = ArrayList<String>()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -24,10 +29,33 @@ class ListFragment : Fragment() {
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
-
+        sqlVeriAlma()
     }
 
     fun sqlVeriAlma() {
+        try {
 
+            activity?.let {
+                val db = it.openOrCreateDatabase("posts", Context.MODE_PRIVATE, null)
+                val cursor = db.rawQuery("SELECT * FROM posts", null)
+                val postBaslikIndex = cursor.getColumnIndex("postBaslik")
+                val postIdIndex = cursor.getColumnIndex("id")
+
+                postIdListesi.clear()
+                postBaslikListesi.clear()
+
+                while (cursor.moveToNext()) {
+
+                    postIdListesi.add(cursor.getInt(postIdIndex))
+                    postBaslikListesi.add(cursor.getString(postBaslikIndex))
+
+                }
+
+                cursor.close()
+            }
+
+        } catch (e : Exception) {
+            e.printStackTrace()
+        }
     }
 }
---
+++ app/src/main/java/com/badlogic/androidgames/addpost/ListRecyclerAdapter.kt
@@ -0,0 +1,24 @@
+package com.badlogic.androidgames.addpost
+
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+
+class ListRecyclerAdapter : RecyclerView.Adapter<ListRecyclerAdapter.PostHolder>() {
+
+    class PostHolder(itemView: View) : RecyclerView.ViewHolder(itemView) {
+
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): PostHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun onBindViewHolder(holder: PostHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int {
+        TODO("Not yet implemented")
+    }
+}
